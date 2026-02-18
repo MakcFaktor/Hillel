232e27e0e09e4c6c7e5dd71502ab034ac2ffa177
Maksym Motruk <motrukmaksim01@gmail.com>
2026-02-18 17:53:00 +0200

    first v

--- scripts/form.js
+++ scripts/form.js
@@ -1,33 +1,33 @@
-const MODAL_ACTIVE_CLASS_NAME = "modal-active";
+document.addEventListener("DOMContentLoaded", () => {
+  const MODAL_ACTIVE_CLASS_NAME = "modal-active";
 
-const formModal = document.querySelector("#form-modal");
-const successModal = document.querySelector("#success-modal");
-const form = document.querySelector("#form");
+  const formModal = document.querySelector("#form-modal");
+  const openFormModalBtn = document.querySelector("#open-form-modal-btn");
+  const closeBtns = document.querySelectorAll(".close-btn");
+  const pageLoader = document.getElementById("pageLoader");
 
-const openFormModalBtn = document.querySelector("#open-form-modal-btn");
-const launchBtn = document.querySelector("#launch-btn");
-const closeBtns = document.querySelectorAll(".close-btn");
+  if (!pageLoader) {
+    console.error("pageLoader НЕ знайдено в HTML");
+    return;
+  }
 
-openFormModalBtn.addEventListener("click", () => {
-  formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-});
+  openFormModalBtn.addEventListener("click", () => {
+    pageLoader.classList.add("active");
 
-const closeFormModal = () => {
-  formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-};
+    setTimeout(() => {
+      pageLoader.classList.remove("active");
+      formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+    }, 800);
+  });
 
-const closeSuccessModal = () => {
-  successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-};
+  const closeFormModal = () => {
+    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+  };
 
-const openSuccessModal = () => {
-  successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-};
-
-closeBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.stopPropagation();
-    closeFormModal();
-    closeSuccessModal();
+  closeBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      closeFormModal();
+    });
   });
 });
